8db956f1268c80420e72b899c31fce8a1e97fa5f
Aryan basu <aryanbasu005@gmail.com>
2021-11-19 00:18:42 +0530

    Home screen

--- screens/Homescreen.js
+++ screens/Homescreen.js
@@ -1,14 +1,46 @@
 /* eslint-disable prettier/prettier */
 // eslint-disable-next-line prettier/prettier
 import React from 'react';
-import {View , Text, Button, StyleSheet} from 'react-native';
-
+import {View , Text, Button, StyleSheet,ScrollView,TouchableOpacity} from 'react-native';
+import { Card,Image } from 'react-native-elements';
+import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
+import AntDesign from 'react-native-vector-icons/AntDesign';
 const Homescreen = ({navigation}) => {
     return (
-        <View style ={styles.container}>
-            <Text>Home Screen</Text>
-            <Button title="Click Here" onPress={() => alert('Button Clicked')} />
-
+        <View style ={{ backgroundColor: '#f5f5f5',marginBottom:80}}>
+            <ScrollView>
+             <Text style={styles.title}>POPULAR ARTICLES</Text>
+        
+             <TouchableOpacity style={{flexDirection:"row",marginLeft:290}}>
+         
+            <Text style={{textAlign:"right",marginRight:5}}>View All</Text>
+            <Icon name="chevron-right-circle" style={{marginLeft:5,color:"#118b06",textAlign:"right"}} size={22} color="#118b06" />
+            </TouchableOpacity>
+     
+      
+   <Card>
+        <Card.Image source={require("../images/interview.png")}></Card.Image>
+        </Card>
+      
+       <TouchableOpacity style={styles.card}>
+                <Text style={styles.cardtitle}>Flipkart Interview</Text>
+                <Text style={{  color: "#767676",
+        fontFamily: "Montserrat_400Regular", marginLeft:10,marginBottom:14,marginTop:10,marginRight:1,justifyContent:"space-evenly"}} numberOfLines={4}>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to mak</Text>
+              
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.card}>
+                <Text style={styles.cardtitle}>Google SDE-I Interview Experience</Text>
+                <Text style={{  color: "#767676",
+        fontFamily: "Montserrat_400Regular", marginLeft:10,marginBottom:14,marginTop:10,marginRight:1,justifyContent:"space-evenly"}} numberOfLines={4}>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to mak</Text>
+              
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.card}>
+                <Text style={styles.cardtitle}>MySql important questions</Text>
+                <Text style={{  color: "#767676",
+        fontFamily: "Montserrat_400Regular", marginLeft:10,marginBottom:14,marginTop:10,marginRight:1,justifyContent:"space-evenly"}} numberOfLines={4}>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to mak</Text>
+              
+        </TouchableOpacity>
+      </ScrollView>
         </View>
     );
 };
@@ -18,9 +50,51 @@ const styles = StyleSheet.create({
 
     container :{
         flex:1,
-        alignItems:'center',
-        justifyContent: 'center',
+        
         backgroundColor: '#f5f5f5',
     },
+    title: {
+        fontFamily: "Montserrat_700Bold",
+        marginTop: 0,
+        marginLeft:20,
+        marginTop:20,
+        marginHorizontal: 10,
+        fontSize: 22,
+        marginBottom:10
+    
+      },
+      card:{
+        marginTop:15,
+        marginLeft:20,
+        height:140,
+        width:350,
+        backgroundColor:"#fff",
+        borderRadius:10,
+        borderColor:"#118b06",
+        borderWidth:2,
+    },
+    text: {
+        color: "#767676",
+        fontFamily: "Montserrat_400Regular",
+        marginTop: 10,
+        marginBottom:10,
+        fontSize: 19,
+        lineHeight: 24,
+        marginLeft: 10,
+       
+      },
+    cardtitle:{
+        fontFamily:"Montserrat_700Bold",
+        color:"#000",
+        marginTop:16,
+      marginLeft:10,
+        fontSize:14,
+        
+              },
+              img:{
+height:200,
+width:470,
+borderRadius:15,
+              }
 
 });
