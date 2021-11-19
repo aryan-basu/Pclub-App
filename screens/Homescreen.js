0f0fc58687a1c0c5766ff5e8d0d0c6b9f3f4ebbb
Aryan basu <aryanbasu005@gmail.com>
2021-11-19 12:43:43 +0530

    image croiusel

--- screens/Homescreen.js
+++ screens/Homescreen.js
@@ -1,26 +1,43 @@
 /* eslint-disable prettier/prettier */
 // eslint-disable-next-line prettier/prettier
 import React from 'react';
-import {View , Text, Button, StyleSheet,ScrollView,TouchableOpacity} from 'react-native';
-import { Card,Image } from 'react-native-elements';
+import {View , Text, Button, StyleSheet,ScrollView,TouchableOpacity,Image,TouchableHighlight} from 'react-native';
+
+import { Card,Slider } from 'react-native-elements';
 import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
 import AntDesign from 'react-native-vector-icons/AntDesign';
 const Homescreen = ({navigation}) => {
     return (
         <View style ={{ backgroundColor: '#f5f5f5',marginBottom:80}}>
+           
             <ScrollView>
              <Text style={styles.title}>POPULAR ARTICLES</Text>
-        
+          
              <TouchableOpacity style={{flexDirection:"row",marginLeft:290}}>
          
             <Text style={{textAlign:"right",marginRight:5}}>View All</Text>
             <Icon name="chevron-right-circle" style={{marginLeft:5,color:"#118b06",textAlign:"right"}} size={22} color="#118b06" />
             </TouchableOpacity>
+         
      
-      
-   <Card>
-        <Card.Image source={require("../images/interview.png")}></Card.Image>
-        </Card>
+          
+
+        <ScrollView  showsHorizontalScrollIndicator={false}  horizontal={true} style={{marginBottom:20,marginTop:20}}>
+        <TouchableOpacity>
+          <Image style={{height:200,width:360,borderRadius:15,marginLeft:20,marginRight:10}} source={require("../images/sql.png")}></Image></TouchableOpacity>
+          <TouchableOpacity>
+        
+          <Image style={{height:200,width:360,borderRadius:15,marginLeft:10,marginRight:20}} source={require("../images/interview.png")}></Image></TouchableOpacity>
+ <TouchableOpacity>
+          <Image style={{height:200,width:360,borderRadius:15,marginLeft:10,marginRight:20}} source={require("../images/Django.jpg")}></Image>
+          </TouchableOpacity>
+  <TouchableOpacity>
+          <Image style={{height:200,width:360,borderRadius:15,marginLeft:10,marginRight:20}} source={require("../images/react.png")}></Image> 
+</TouchableOpacity>
+<TouchableOpacity>
+         <Image style={{height:200,width:360,borderRadius:15,marginLeft:10,marginRight:20}} source={require("../images/blockchain.png")}></Image> 
+         </TouchableOpacity>
+          </ScrollView>
       
        <TouchableOpacity style={styles.card}>
                 <Text style={styles.cardtitle}>Flipkart Interview</Text>
