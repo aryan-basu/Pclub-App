95bbcf1a4ffa8c470fe15071e3cd47ea9e6d0333
Aryan basu <aryanbasu005@gmail.com>
2021-11-25 15:15:24 +0530

    Update PsoC.js

--- screens/PsoC.js
+++ screens/PsoC.js
@@ -3,6 +3,7 @@ import {View , Text, Button , StyleSheet,ScrollView,TouchableOpacity,Image} from
 import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
 import AntDesign from 'react-native-vector-icons/AntDesign';
 import YouTubePlayer from "react-native-youtube-sdk";
+import {widthPercentageToDP as wp, heightPercentageToDP as hp} from 'react-native-responsive-screen';
 const PsoC = () => {
 
     return (
@@ -16,8 +17,8 @@ const PsoC = () => {
     
     </TouchableOpacity>
     <TouchableOpacity style={{marginTop:20}}>
-    <Image style={{height:350,width:370,borderRadius:15,marginLeft:10}} source={require("../images/psocmain.jpg")}></Image></TouchableOpacity>
-    <Text style={{marginTop:20,marginLeft:10,marginRight:10,textAlign:"center",fontFamily:"Montserrat_400Regular",fontSize:14,color: "#767676",lineHeight:22}}>
+    <Image style={{height:hp(48),width:wp(94),borderRadius:15,marginLeft:wp(2.7)}} source={require("../images/psocmain.jpg")}></Image></TouchableOpacity>
+    <Text style={{marginTop:hp(2),marginLeft:wp(1),marginRight:wp(1),textAlign:"center",fontFamily:"Montserrat_400Regular",fontSize:14,color: "#767676",lineHeight:22}}>
     PClub Summer of Code is the 2 month long Open Source program during summers conducted by Programming Club, UIET, with an aim to help beginners get started with Open Source Development while encouraging diversity. Throughout the program, participants contribute to different projects under guidance of experienced mentors. Mentors get to make their ideas alive and Mentees get to learn tech.
 
 PSoC is targeted at audience who want to dwell in OpenSource Development as well as who are already into FOSS development. This program will help students for other opensource programs like Gsoc, Outreachy, etc. </Text>
@@ -26,15 +27,15 @@ PSoC is targeted at audience who want to dwell in OpenSource Development as well
          
          <ScrollView  showsHorizontalScrollIndicator={false}  horizontal={true} style={{marginBottom:20,marginTop:20}}>
                  <TouchableOpacity>
-                   <Image style={{height:350,width:350,borderRadius:15,marginLeft:20,marginRight:10}} source={require("../images/psocsponser1.jpg")}></Image></TouchableOpacity>
+                   <Image style={{height:hp(45.5),width:wp(87),borderRadius:15,marginLeft:wp(6.5),marginRight:wp(3)}} source={require("../images/psocsponser1.jpg")}></Image></TouchableOpacity>
                  
                  
                    <TouchableOpacity>
-                   <Image style={{height:350,width:350,borderRadius:15,marginLeft:20,marginRight:10}} source={require("../images/psocsponser2.jpg")}></Image></TouchableOpacity>
+                   <Image style={{height:hp(45.5),width:wp(87),borderRadius:15,marginLeft:wp(6.5),marginRight:wp(3)}} source={require("../images/psocsponser2.jpg")}></Image></TouchableOpacity>
                    <TouchableOpacity>
-                   <Image style={{height:350,width:350,borderRadius:15,marginLeft:20,marginRight:10}} source={require("../images/psocsponser3.jpg")}></Image></TouchableOpacity>
+                   <Image style={{height:hp(45.5),width:wp(87),borderRadius:15,marginLeft:wp(6.5),marginRight:wp(3)}} source={require("../images/psocsponser3.jpg")}></Image></TouchableOpacity>
                    <TouchableOpacity>
-                   <Image style={{height:350,width:350,borderRadius:15,marginLeft:20,marginRight:10}} source={require("../images/psocsponser4.jpg")}></Image></TouchableOpacity>
+                   <Image style={{height:hp(45.5),width:wp(87),borderRadius:15,marginLeft:wp(4.5),marginRight:wp(8)}} source={require("../images/psocsponser4.jpg")}></Image></TouchableOpacity>
     
          
                  </ScrollView>
