aef05b1946d6069c719e80351cacdfd263956050
Aryan basu <aryanbasu005@gmail.com>
2021-11-25 16:16:34 +0530

    Update Sfd.js

--- screens/Sfd.js
+++ screens/Sfd.js
@@ -3,37 +3,37 @@ import {View , Text, Button , StyleSheet,ScrollView,TouchableOpacity,Image} from
 import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
 import AntDesign from 'react-native-vector-icons/AntDesign';
 import YouTubePlayer from "react-native-youtube-sdk";
-
+import {widthPercentageToDP as wp, heightPercentageToDP as hp} from 'react-native-responsive-screen';
 const Sfd = () => {
 
     return (
         <ScrollView   style={{marginBottom:20,marginTop:20}}>
             <TouchableOpacity style={{flexDirection:"row"}} onPress={()=>navigation.navigate('Explore')}>
-    <Icon name="arrow-left" style={{marginTop:0,marginLeft:15,color:"#118b06",textAlign:"right",marginRight:10}} size={28} color="#118b06" />
+    <Icon name="arrow-left" style={{marginTop:hp(1),marginLeft:wp(2),color:"#118b06",textAlign:"right",marginRight:wp(3)}} size={28} color="#118b06" />
     <Text style={styles.title}>Software Freedom Day</Text> 
     
     </TouchableOpacity>
     <TouchableOpacity style={{marginTop:20}}>
-    <Image style={{height:200,width:370,borderRadius:15,marginLeft:10,marginRight:10}} source={require("../images/sfdmain.jpg")}></Image></TouchableOpacity>
+    <Image style={{height:hp(25),width:wp(94),borderRadius:15,marginLeft:wp(3),marginRight:wp(2)}} source={require("../images/sfdmain.jpg")}></Image></TouchableOpacity>
 
-<Text style={{marginTop:20,marginLeft:10,marginRight:10,textAlign:"center",fontFamily:"Montserrat_400Regular",fontSize:14,color: "#767676",lineHeight:22}}>Software Freedom Day is a worldwide event to celebrate the usage of Free and Open Source Software. It is a public education effort to increase awareness of free software and its virtues and encouraging its use. Each year the event has seen a growing number of participants. Our past speakers were from many prestigious software companies like Facebook, Microsoft and Google, enlightening us about the various software developments. SFD aims to bring together developers and students, of all levels, on a single platform to share their thoughts, ideas and inspirations.</Text>
+<Text style={{marginTop:hp(3),marginLeft:wp(2),marginRight:wp(2),marginBottom:hp(2),textAlign:"center",fontFamily:"Montserrat_400Regular",fontSize:14,color: "#767676",lineHeight:22}}>Software Freedom Day is a worldwide event to celebrate the usage of Free and Open Source Software. It is a public education effort to increase awareness of free software and its virtues and encouraging its use. Each year the event has seen a growing number of participants. Our past speakers were from many prestigious software companies like Facebook, Microsoft and Google, enlightening us about the various software developments. SFD aims to bring together developers and students, of all levels, on a single platform to share their thoughts, ideas and inspirations.</Text>
 
 
 <Text style={styles.title}>Our Speakers</Text>
 
-<ScrollView  showsHorizontalScrollIndicator={false}  horizontal={true} style={{marginBottom:20,marginTop:20}}>
+<ScrollView  showsHorizontalScrollIndicator={false}  horizontal={true} style={{marginBottom:hp(3),marginTop:hp(3)}}>
         <TouchableOpacity>
-          <Image style={{height:350,width:350,borderRadius:15,marginLeft:20,marginRight:10}} source={require("../images/speaker1.jpg")}></Image></TouchableOpacity>
+          <Image style={{height:hp(46),width:wp(88),borderRadius:15,marginLeft:wp(6),marginRight:wp(4)}} source={require("../images/speaker1.jpg")}></Image></TouchableOpacity>
         
         
           <TouchableOpacity>
-          <Image style={{height:350,width:350,borderRadius:15,marginLeft:20,marginRight:10}} source={require("../images/speaker2.jpg")}></Image></TouchableOpacity>
+          <Image style={{height:hp(46),width:wp(88),borderRadius:15,marginLeft:wp(6),marginRight:wp(4)}} source={require("../images/speaker2.jpg")}></Image></TouchableOpacity>
           <TouchableOpacity>
-          <Image style={{height:350,width:350,borderRadius:15,marginLeft:20,marginRight:10}} source={require("../images/speaker3.jpg")}></Image></TouchableOpacity>
+          <Image style={{height:hp(46),width:wp(88),borderRadius:15,marginLeft:wp(6),marginRight:wp(4)}} source={require("../images/speaker3.jpg")}></Image></TouchableOpacity>
           <TouchableOpacity>
-          <Image style={{height:350,width:350,borderRadius:15,marginLeft:20,marginRight:10}} source={require("../images/speaker4.jpg")}></Image></TouchableOpacity>
+          <Image style={{height:hp(46),width:wp(88),borderRadius:15,marginLeft:wp(6),marginRight:wp(4)}} source={require("../images/speaker4.jpg")}></Image></TouchableOpacity>
           <TouchableOpacity>
-          <Image style={{height:350,width:350,borderRadius:15,marginLeft:20,marginRight:10}} source={require("../images/speaker5.jpg")}></Image></TouchableOpacity>
+          <Image style={{height:hp(46),width:wp(88),borderRadius:15,marginLeft:wp(6),marginRight:wp(7)}} source={require("../images/speaker5.jpg")}></Image></TouchableOpacity>
 
         </ScrollView>
 
@@ -144,7 +144,7 @@ const Sfd = () => {
   onError={e => console.log(e)}
   onChangeState={e => console.log(e)}
   onChangeFullscreen={e => console.log(e)}
-/>
+/> 
           </ScrollView>
     );
 };
@@ -164,7 +164,7 @@ const styles = StyleSheet.create ({
       },
       title: {
         fontFamily: "Montserrat_700Bold",
-        marginTop:20,
+        marginTop:hp(1),
         textAlign:"center",
         fontSize: 22,
     
