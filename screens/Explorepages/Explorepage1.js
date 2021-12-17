497621ed8dd5808d8cbb6b56f6dd7bff05a11caf
Aryan basu <aryanbasu005@gmail.com>
2021-12-17 23:10:16 +0530

    Update Explorepage1.js

--- screens/Explorepages/Explorepage1.js
+++ screens/Explorepages/Explorepage1.js
@@ -1,14 +1,44 @@
 import React from "react";
-import {View , Text, Button , StyleSheet,TouchableOpacity,Image,ScrollView} from 'react-native';
+import {View , Text, Button , StyleSheet,TouchableOpacity,Image,ScrollView,ActivityIndicator,Linking} from 'react-native';
 import { Card } from "react-native-elements/dist/card/Card";
-
+import firestore from '@react-native-firebase/firestore';
+import { useState,useEffect } from 'react';
 import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
 import AntDesign from 'react-native-vector-icons/AntDesign';
 import FontAwesome from 'react-native-vector-icons/FontAwesome';
 import {widthPercentageToDP as wp, heightPercentageToDP as hp} from 'react-native-responsive-screen';
 const Explorepage1 = ({navigation}) => {
+  const [loader,setloader]=useState(true);
+    const articles=[];
+    const [searchTerm,setsearchTerm]=useState("");
+    const [books, setBooks] = useState(null);
+    useEffect(() => {
+        getData();
 
-    return (
+        // we will use async/await to fetch this data
+        async function getData() {
+          
+           await firestore().collection('Project').get().then((querySnapshot) => {
+                querySnapshot.forEach((doc) => {
+                    // doc.data() is never undefined for query doc snapshots
+                 // console.log(doc.data().email);
+                   articles.push({Title:doc.data().name,descriptions:doc.data().descriptions,github:doc.data().github});
+                    //console.log(doc.id, " => ", doc.data().firstName);
+                //  console.log(doc.data().id);
+                });
+               // console.log(maindata);
+               // <CSVDownload data={maindata} target="_blank" />
+              
+              });
+   //console.log(articles)
+          // store the data into our books variable
+          //console.log(maindata);
+         setBooks(articles) ;
+         setloader(false)
+        
+        }
+      }, []);
+    return loader?(<View style={{justifyContent:"center",flex: 1,}}><ActivityIndicator size="large" color="#118b06" /></View>):(
         <ScrollView>
 <View style={styles.container}>
     <View style={{flexDirection:"row"}} >
@@ -16,14 +46,22 @@ const Explorepage1 = ({navigation}) => {
     <Text style={styles.title}>Project</Text> 
     
     </View>
-    
+    {books && ( 
+         <ScrollView>
+  {books.filter((val)=>{if(searchTerm===""){
+              return val
+          }else if(val.dish.toLowerCase().includes(searchTerm.toLowerCase())){
+        return val
+          }
+        }).map((book , index) => index<105&&( 
+            <View key={index}>
     <TouchableOpacity style={[styles.card2,styles.elevation,{marginTop:20}]}>
-        <Text style={styles.cardtitle}>Pclub-Meet</Text>
+        <Text style={styles.cardtitle}>{book.Title}</Text>
         <Text style={{fontFamily:"Montserrat_400Regular",
 color:"#767676",
 marginTop:hp(2),
 textAlign:"center",marginLeft:wp(2),marginRight:wp(2),
-fontSize:14,}}>Simple Web Application that offer you to create video meeting room using WebRTC and Socket its a project announced by Pclub to meet their needs</Text>
+fontSize:14,}}>{book.descriptions}</Text>
         <View style={{flexDirection:"row",justifyContent:"center",marginTop:hp(12)}}>
         <FontAwesome name="envelope-square" style={{marginTop:hp(2.6),marginLeft:0,color:"#118b06"}} size={38} color="#118b06" />
         <FontAwesome name="github-square" style={{marginTop:hp(2.6),marginLeft:wp(2),color:"#118b06"}} size={38} color="#118b06" />
@@ -31,21 +69,12 @@ fontSize:14,}}>Simple Web Application that offer you to create video meeting roo
         </View>
         
     </TouchableOpacity>
-    <TouchableOpacity style={[styles.card2,styles.elevation,{marginTop:hp(6),marginBottom:hp(6)}]}>
-        <Text style={styles.cardtitle}>!answer</Text>
-        <Text style={{fontFamily:"Montserrat_400Regular",
-color:"#767676",
-marginTop:hp(2),
-textAlign:"center",marginLeft:wp(2),marginRight:wp(2),
-fontSize:14,}}>!answer is a discord bot that leverages state of the art artifical intelligence to serve your users in your discord guild. You can give the bot context paragraphs, from which the bot will reply to any questions of users. Best used to answer a user's doubt about FAQs or rules or venue.
-!answer also plans to release moderation tools and a real time dashboard for the ease of discord admins and moderators</Text>
-        <View style={{flexDirection:"row",justifyContent:"center",marginTop:hp(2)}}>
-        <FontAwesome name="envelope-square" style={{marginTop:hp(2.6),marginLeft:0,color:"#118b06"}} size={38} color="#118b06" />
-        <FontAwesome name="github-square" style={{marginTop:hp(2.6),marginLeft:wp(2),color:"#118b06"}} size={38} color="#118b06" />
-        <FontAwesome name="linkedin-square" style={{marginTop:hp(2.6),marginLeft:wp(2),color:"#118b06",}} size={38} color="#118b06" />
-        </View>
-        
-    </TouchableOpacity>
+    </View>
+                ))}
+
+                </ScrollView>
+                    )}
+    
               
                 
              {/*   <Image style={{height:350,width:380,marginTop:40,marginLeft:10,justifyContent:"center"}} source={require("../../images/NotFound.png")}></Image>  */}
