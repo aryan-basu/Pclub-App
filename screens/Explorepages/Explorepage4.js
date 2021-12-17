c7b5684fb6f135ca6a780d1794e726bbf57e0544
Aryan basu <aryanbasu005@gmail.com>
2021-12-17 22:26:44 +0530

    Update Explorepage4.js

--- screens/Explorepages/Explorepage4.js
+++ screens/Explorepages/Explorepage4.js
@@ -1,13 +1,45 @@
 import React from "react";
-import {View , Text, Button , StyleSheet,TouchableOpacity,Image,ScrollView,Linking} from 'react-native';
+import {View , Text, Button , StyleSheet,TouchableOpacity,Image,ScrollView,ActivityIndicator,Linking} from 'react-native';
 import { Card } from "react-native-elements/dist/card/Card";
 import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
 import AntDesign from 'react-native-vector-icons/AntDesign';
 import {widthPercentageToDP as wp, heightPercentageToDP as hp} from 'react-native-responsive-screen';
+import firestore from '@react-native-firebase/firestore';
+import { useState,useEffect } from 'react';
 import YouTubePlayer from "react-native-youtube-sdk";
 const Explorepage4 = ({navigation}) => {
+    const [loader,setloader]=useState(true);
+    const articles=[];
+    const [searchTerm,setsearchTerm]=useState("");
+    const [books, setBooks] = useState(null);
+    useEffect(() => {
+        getData();
 
-    return (
+        // we will use async/await to fetch this data
+        async function getData() {
+          
+           await firestore().collection('AIsaturday').get().then((querySnapshot) => {
+                querySnapshot.forEach((doc) => {
+                    // doc.data() is never undefined for query doc snapshots
+                 // console.log(doc.data().email);
+                   articles.push({Title:doc.data().title,videoid:doc.data().videoId});
+                    //console.log(doc.id, " => ", doc.data().firstName);
+                //  console.log(doc.data().id);
+                });
+               // console.log(maindata);
+               // <CSVDownload data={maindata} target="_blank" />
+              
+              });
+   //console.log(articles)
+          // store the data into our books variable
+          //console.log(maindata);
+         setBooks(articles) ;
+         setloader(false)
+        
+        }
+      }, []);
+
+    return loader?(<View style={{justifyContent:"center",flex: 1,}}><ActivityIndicator size="large" color="#118b06" /></View>):(
 <View style={styles.container}>
     <ScrollView>
 <View style={{flexDirection:"row"}} >
@@ -15,71 +47,41 @@ const Explorepage4 = ({navigation}) => {
     <Text style={styles.title}>AI SATURDAY</Text> 
     </View>
 
-   
-      
-                
-                <Text style={{fontFamily:"Montserrat_700Bold", marginLeft:20,
-        marginTop:20,}}>Introduction to Neutral Network</Text>
+    {books && ( 
+         <ScrollView>
+  {books.filter((val)=>{if(searchTerm===""){
+              return val
+          }else if(val.dish.toLowerCase().includes(searchTerm.toLowerCase())){
+        return val
+          }
+        }).map((book , index) => index<105&&( 
+            <View key={index}>
+        <Text style={{fontFamily:"Montserrat_700Bold", marginLeft:wp(6),
+        marginTop:hp(3),}}>{book.Title}</Text>
                
-                
-                <YouTubePlayer
-  ref={ref => (this.youTubePlayer = ref)}
-  videoId="FUov71Xqtkw"
-  autoPlay={false}
-  fullscreen={false}
-  showFullScreenButton={true}
-  showSeekBar={true}
-  showPlayPauseButton={true}
-  startTime={5}
-  style={{ width: "95%", height: hp(27),marginBottom:hp(2),marginTop:hp(2),marginLeft:wp(2),marginRight:wp(4)}}
-  onError={e => console.log(e)}
-  onChangeState={e => console.log(e)}
-  onChangeFullscreen={e => console.log(e)}
-/>
-            
-                <Text style={{fontFamily:"Montserrat_700Bold", marginLeft:20,
-        marginTop:20,}}>Optimizing Neutral Network</Text>        
-            
-                <YouTubePlayer
-  ref={ref => (this.youTubePlayer = ref)}
-  videoId="dw1-kkVTIM4"
-  autoPlay={false}
-  fullscreen={false}
-  showFullScreenButton={true}
-  showSeekBar={true}
-  showPlayPauseButton={true}
-  startTime={5}
-  style={{ width: "95%", height: hp(27),marginBottom:hp(2),marginTop:hp(2),marginLeft:wp(2),marginRight:wp(4)}}
-  onError={e => console.log(e)}
-  onChangeState={e => console.log(e)}
-  onChangeFullscreen={e => console.log(e)}
-/>
-                
-                <Text style={{fontFamily:"Montserrat_700Bold", marginLeft:20,
-        marginTop:20}}>Introduction To CNN</Text>
-              
-                
-                <YouTubePlayer
-  ref={ref => (this.youTubePlayer = ref)}
-  videoId="hR_Mjm6LVuQ"
-  autoPlay={false}
-  fullscreen={false}
-  showFullScreenButton={true}
-  showSeekBar={true}
-  showPlayPauseButton={true}
-  startTime={5}
-  style={{ width: "95%", height: hp(27),marginBottom:hp(2),marginTop:hp(2),marginLeft:wp(2),marginRight:wp(4)}}
-  onError={e => console.log(e)}
-  onChangeState={e => console.log(e)}
-  onChangeFullscreen={e => console.log(e)}
-/>
              
-
-
-
-               
+                <YouTubePlayer
+  ref={ref => (this.youTubePlayer = ref)}
+  videoId={book.videoid}
+  autoPlay={false}
+  fullscreen={false}
+  showFullScreenButton={true}
+  showSeekBar={true}
+  showPlayPauseButton={true}
+  startTime={5}
+  style={{ width: "95%", height: hp(27),marginBottom:hp(2),marginTop:hp(2),marginLeft:wp(2),marginRight:wp(4)}}
+  onError={e => console.log(e)}
+  onChangeState={e => console.log(e)}
+  onChangeFullscreen={e => console.log(e)}
+/>
+</View>
+                ))}
 
                 </ScrollView>
+                    )}
+      
+               </ScrollView> 
+              
 </View>
     );
 };
